15627b952a7249c465e4385291c34d1bbee9e07a
Shaun Ostoic <ostoic@proton.me>
2024-10-23 19:00:12 -0400

    msys2: Use nsswitch.conf method of switching home dirs

--- msys2/setup-scoop-persist-dir.sh
+++ msys2/setup-scoop-persist-dir.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Handle POSIX symlinks using Windows equivalents (i.e., Junction or SymbolicLink)
-export MSYS=winsymlinks:native
-
 if [ ! -d /c/Users/$USER/scoop/persist/msys2/ ] ; then
 	echo "Error: MSYS2 not installed!"
 	exit -1
@@ -12,4 +9,7 @@ if [ ! -d /c/Users/$USER/scoop/persist/msys2/home/$USER ]; then
 	# Link Windows home directory to scoop MSYS2 persist home directory
 	powershell -c "New-Item -ItemType Junction -Name C:\\Users\\$USER\\scoop\\persist\\msys2\\home\\$USER -Target C:\\Users\\$USER"
 fi
+
+sed -i 's|db_home: cygwin desc|db_home: windows|g' /etc/nsswitch.conf
+
 
